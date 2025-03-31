6284381b262cfb89fda1d9086b48eb8dd898b4c9
Minnie Nguyen <minniexnguyen@gmail.com>
2025-03-31 10:31:16 +0100

    Change save path

--- src/autoencoder_base.py
+++ src/autoencoder_base.py
@@ -655,7 +655,7 @@ def train_pipeline(train_loader, val_loader, test_loader, device, run_path,
 
 train_pipeline(
     train_loader, val_loader, test_loader,
-    device, "runs/autoencoder/augmented_weighted_20epochs",
+    device, "runs/autoencoder/base_20epochs",
     autoencoder_epochs=20,
     segmentation_epochs=20
 )
